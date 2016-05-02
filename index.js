5a98fd6602b82a287e13a7915d541c13f7d07a4f
hrushikesh09 <hrushikeshshinde11@yahoo.com>
2016-05-02 20:44:20 +0530

    index.js modified

--- index.js
+++ index.js
@@ -7,13 +7,15 @@ app.set('port', (process.env.PORT || 5000));
 app.get('/', function(req, res) {
 
   var ip = req.connection.remoteAddress;
+  ip = ip.split(":");
+  //var ip = req.headers['x-forwarded-for'];
   var sw = req.headers['user-agent'].match('\(([^\)]+)\)');
   sw = sw[1].split('(');
   sw = sw[1].replace(/;/g, ',');
   var language = req.headers['accept-language'].split(',');
 
   var json = {
-    ipaddress: ip,
+    ipaddress: ip[ip.length - 1],
     language: language[0],
     software: sw
   }
